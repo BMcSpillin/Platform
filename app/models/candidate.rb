b38d7c7aa532a11bae4ad51565d96844f28f7186
Bridget McMillan <BridgetMc09@gmail.com>
2016-04-13 14:42:15 -0400

    Update candidate.rb

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -22,11 +22,11 @@ end
   #   # @zip4 = @user.zip4
 
   #   # Call and parse API to get STATS about candidate
-  #   @candidate = HTTParty.get "http://api.votesmart.org/Candidates.getByZip?key=faa57400e6415f036d90a5089e7e6603&zip5=#{@zip5}"
+  #   @candidate = HTTParty.get "http://api.votesmart.org/Candidates.getByZip?key=#{ENV['VOTESMART_API_KEY']}&zip5=#{@zip5}"
   #   @candidateHash = @candidate.parsed_response["candidateList"]["candidate"]
   #   #Set candidate ID to call other relevant hashes from the API
   #   @Id = @candidateHash[@@i]["candidateId"]
   #   # Call and parse API to get candidate BIO
-  #   @candidateBio = HTTParty.get "http://api.votesmart.org/CandidateBio.getBio?key=faa57400e6415f036d90a5089e7e6603&candidateId=#{@Id}"
+  #   @candidateBio = HTTParty.get "http://api.votesmart.org/CandidateBio.getBio?key=#{ENV['VOTESMART_API_KEY']}&candidateId=#{@Id}"
   #   @candidateBioHash = @candidateBio.parsed_response["bio"]["candidate"]
   #   @candidatePhoto = @candidateBioHash["photo"]
