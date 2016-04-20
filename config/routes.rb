e72e915901842255bf831b00745cf43067c46b2e
Bridget McMillan <bridgetmc09@gmail.com>
2016-04-19 21:57:06 -0400

    everything in its right place

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   post '/location' => 'users#create', as: :location
 
+  get '/userZip' => 'users#zip', as: :zip
+
   get '/candidates' => 'users#candidates', as: :candidates
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
