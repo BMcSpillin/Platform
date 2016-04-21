943d6c5c06f61570f4496884308866b24e02114e
Bridget McMillan <bridgetmc09@gmail.com>
2016-04-21 18:02:51 -0400

    it works!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,68 @@
 class UsersController < ApplicationController
   require 'httParty'
-  require 'cgi'
+  require 'smartystreets'
+  require 'uri'
+  require 'json'
+  require 'net/https'
   
   def index
-    # @user = User.new
-    @address = ""
+    
   end
 
   # routed as "location"
   def create
-    # SmartyStreets.set_auth("#{ENV['SMARTYSTREETS_AUTH_ID']}", "#{ENV['SMARTYSTREETS_AUTH_TOKEN']}")
-    redirect_to zip_path
+    @user = User.create(params[:id])
+    @user.address = params[:user][:address]
+    @user.save!
+    
+    if @user.save 
+      redirect_to zip_path
+    else
+      redirect_to root_path
+    end
   end
 
   def zip
-    # cgi = CGI.new
-    # urlFriendlyStreet = cgi.escape(params["userAddress"]) 
-    @entered = params["userAddress"]
-    SmartyStreets::StreetAddressApi.call(
-      SmartyStreets::StreetAddressRequest.new(
-        :street => "#{CGI::escape("2313 38th st long island city ny")}"
-      )
-    )
+    @user = User.last
+    @authID = URI.encode(ENV['SMARTYSTREETS_AUTH_ID'])
+    @authToken = URI.encode(ENV['SMARTYSTREETS_AUTH_TOKEN'])
 
-    @zip5 = SmartyStreets::StreetAddressResponse::Components.zipcode
-    @zip4 = SmartyStreets::StreetAddressResponse::Components.plus4_code
-    @user = User.create(@zip5, @zip4)
-    # puts @zipcode
-    # @user = User.create(
-    #   :zip5 => userAddress.Components[@zipcode]
-    #   :zip4 => userAddress.Components[@plus4_code]
-    #   )
-    redirect_to candidates_path
+    uri = URI('https://api.smartystreets.com/street-address')
+    args = {
+      'street' => "#{@user.address}",
+      'auth-id' => "#{ENV['SMARTYSTREETS_AUTH_ID']}",
+      'auth-token' => "#{ENV['SMARTYSTREETS_AUTH_TOKEN']}"
+    }
+    uri.query = URI.encode_www_form(args)
+
+    response = Net::HTTP.get_response(uri)
+
+    @responseHash = eval("#{JSON.pretty_generate(JSON.parse(response.body))}")
+
+    @user.zip5 = @responseHash[0][:components][:zipcode]
+    @user.zip4 = @responseHash[0][:components][:plus4_code]
+    @user.save!
+    
+    if @user.zip5 != nil
+      redirect_to candidates_path
+    end
+
   end
 
   # routed as "candidates"
   def candidates
-    @user = User.where(id: session[:user_id]).first
-    @zip5 = "79901"
+  # This will work as long as only one person at a time uses the app :-/
+    @user = User.last
+    @zip5 = @user.zip5
+    @zip4 = @user.zip4
 
   # Call and parse API to get STATS about candidate
-    @candidates = HTTParty.get "http://api.votesmart.org/Candidates.getByZip?key=#{ENV['VOTESMART_API_KEY']}&zip5=#{@zip5}"
+    @candidates = HTTParty.get "http://api.votesmart.org/Candidates.getByZip?key=#{ENV['VOTESMART_API_KEY']}&zip5=#{@zip5}&zip4=#{@zip4}&electionYear=2014"
     @candArray = @candidates["candidateList"]["candidate"]
 
   # Run through candidate array indices
-    @i = 0
+      @i = 0
+
     while @i < @candArray.size
       # Set candidate ID to call other relevant hashes from the API
       @Id = @candArray[@i]["candidateId"]
@@ -52,26 +70,22 @@ class UsersController < ApplicationController
       @candBio = HTTParty.get "http://api.votesmart.org/CandidateBio.getBio?key=#{ENV['VOTESMART_API_KEY']}&candidateId=#{@Id}"
       @candBioHash = @candBio["bio"]["candidate"]
       @candArray[@i]["photo"] = @candBioHash["photo"]
-  # <!-- For Votes -->    
-      @candidateVotes = HTTParty.get "http://api.votesmart.org/Votes.getByOfficial?key=#{ENV['VOTESMART_API_KEY']}&candidateId=#{@Id}"
+  # <!-- For Votes in prior offices -->
+        @candidateVotes = HTTParty.get "http://api.votesmart.org/Votes.getByOfficial?key=#{ENV['VOTESMART_API_KEY']}&candidateId=#{@Id}"
       if @candidateVotes != nil
         @candVotesHash = @candidateVotes["bills"]
         @candArray[@i]["votes"] = @candVotesHash
       else
         @candArray[@i]["votes"] = {}
       end
-# @candVotesHash = hash
-# key: "bill", value is an array
-# array contains a hash
-# within that hash are "title" and "vote" keys
+
       @i += 1
     end
-
-      # <!-- For Votes in prior offices -->
 
   end
 
   def user_params
-    params.require(:user).permit(:zip5, :zip4)
+    params.require(:user).permit(:zip5, :zip4, :address)
   end
+
 end
